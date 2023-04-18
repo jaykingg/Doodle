8c4fde5f265858f1db8527027209ba6824231834
jayking <mojunseo49@naver.com>
2023-04-18 19:49:28 +0900

    feat: 뒷큰수 stack 활용

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,5 +2,8 @@ fun main(args: Array<String>) {
     //Solve1().main()
     //Solve2().main()
     //Sovle3().main()
-    Solve5().main()
+    //Solve4().main()
+    //Solve5().main()
+    Solve6().main()
+    //Solve7().main()
 }
---
+++ src/main/kotlin/Solve7.kt
@@ -0,0 +1,7 @@
+class Solve7 {
+    fun main() {
+
+    }
+
+
+}
