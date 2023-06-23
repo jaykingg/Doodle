c501aa5facaa55d6263d34ec94154b29567620ac
jayking <mojunseo49@naver.com>
2023-06-23 20:19:36 +0900

    feat: 후보추천하기(비교연산사용)

--- src/main/kotlin/Solve18.kt
+++ src/main/kotlin/Solve18.kt
@@ -1,11 +1,57 @@
+import java.util.LinkedList
+
 /*
     https://www.acmicpc.net/problem/1713
     후보 추천하기
  */
+
+
 class Solve18 {
+    data class Student(
+        val number: Int,
+        var count: Int,
+        var index: Int
+    )
+
+
     fun main() {
+        val n = readln().toInt()
+        val m = readln().toInt()
+        val recommend = readln().split(" ").map { it.toInt()}.toList()
+        val q = LinkedList<Student>()
 
+        recommend.forEachIndexed { index, student ->
+            val value = q.find { it.number == student }
+            if(q.size < n) {
+                if(value == null) {
+                    q.add(Student(student,1,index))
+                }
+                else {
+                    value.count++
+                }
+            }
+            else {
+                if(value == null) {
+                    q.poll()
+                    q.add(Student(student,1,index))
+                }
+                else {
+                    value.count++
+                }
+            }
 
+            q.sortWith{o1,o2 ->
+                if(o1.count == o2.count) {
+                    o1.index - o2.index
+                }
+                else {
+                    o1.count - o2.count
+                }
+            }
+        }
+
+        val answer = q.map { it.number }.sortedBy { it }.joinToString(" ")
+        println(answer)
     }
 
 }
