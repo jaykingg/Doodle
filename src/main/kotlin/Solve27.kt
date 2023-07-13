d54a3c41e88176d410663146545fcdfbf101d0d6
jayking <mojunseo49@naver.com>
2023-07-13 17:24:30 +0900

    feat: 재귀인줄 알았는데 깜짝 피보나치였던 것

---
+++ src/main/kotlin/Solve27.kt
@@ -0,0 +1,43 @@
+class Solve27 {
+    fun main() {
+        println(solution(4))
+        println(solution(3))
+    }
+
+    var result = 0
+    fun solution(n: Int): Long {
+        return if(n == 1) 1
+        else {
+            val arr: LongArray = LongArray(2001)
+            arr[1] = 1
+            arr[2] = 2
+            for(i in 3 until 2001-2) {
+                arr[i] = (arr[i-1] + arr[i-2])%1234567
+            }
+            arr[n]
+        }
+
+
+
+//        goStep(n,0)
+//        return (result.toLong()%1234567)
+    }
+
+//    private fun goStep(n: Int, count: Int) {
+//        for(i in 1 .. 2) {
+//            val nextCount = count + i
+//            if(nextCount == n) {
+//                result++
+//                return
+//            }
+//            else if(nextCount > n) {
+//                return
+//            }
+//            else {
+//                goStep(n, nextCount)
+//            }
+//        }
+//
+//    }
+
+}
