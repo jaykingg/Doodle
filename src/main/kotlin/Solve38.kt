b39bc3413b30a1ff3cab76d39a71aadfe2ecf9b1
jayking <mojunseo49@naver.com>
2024-02-28 05:35:11 +0900

    solve: Minimum Loss

--- src/main/kotlin/Solve38.kt
+++ src/main/kotlin/Solve38.kt
@@ -1,6 +1,27 @@
-class Solve38 {
-    fun main() {
+import kotlin.math.min
 
+class Solve38 {
+    /*
+    Minimum Loss
+    https://www.hackerrank.com/challenges/minimum-loss/problem?isFullScreen=true
+     */
+    fun main() {
+        val n = readLine()!!.trim().toInt()
+        val price = readLine()!!.trimEnd().split(" ").map { it.toLong() }.toTypedArray()
+        val result = minimumLoss(price)
+        println(result)
+    }
+
+    fun minimumLoss(price: Array<Long>): Int {
+        var result: Long = Long.MAX_VALUE
+        for (i in 0 until price.size - 1) {
+            for (j in i + 1 until price.size) {
+                if (price[i] > price[j]) {
+                    result = min(result, price[i] - price[j])
+                }
+            }
+        }
+        return result.toInt()
     }
 
 }
