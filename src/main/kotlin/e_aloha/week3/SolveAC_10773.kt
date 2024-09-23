3eca33f89c82a3bb03d22172210b00ce561611a4
jayking <mojunseo49@naver.com>
2024-09-23 10:32:41 +0900

    fix:  오탈자 수정

--- src/main/kotlin/e_aloha/week3/SolveAC_10773.kt
+++ src/main/kotlin/e_aloha/week3/SolveAC_10773.kt
@@ -1,6 +1,6 @@
 package e_aloha.week3
 
-import java.util.Stack
+import java.util.*
 
 /**
  * 1st
@@ -10,16 +10,14 @@ import java.util.Stack
  * 0 일 때 Stack 에서 꺼내준다
  */
 fun main() = with(System.`in`.bufferedReader()) {
-    /
     val stack: Stack<Int> = Stack()
     val k = readLine().toInt()
 
     repeat(k) {
         val number = readLine().toInt()
-        if(number == 0 && stack.isNotEmpty()) {
+        if (number == 0 && stack.isNotEmpty()) {
             stack.pop()
-        }
-        else {
+        } else {
             stack.push(number)
         }
     }
