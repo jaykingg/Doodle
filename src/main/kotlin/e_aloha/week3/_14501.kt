d380343bb28d221a0b0e140b124c49b8be8ccf8a
jayking <mojunseo49@naver.com>
2024-10-04 16:50:33 +0900

    feat: 퇴사 문제풀이 중

--- src/main/kotlin/e_aloha/week3/_14501.kt
+++ src/main/kotlin/e_aloha/week3/_14501.kt
@@ -6,17 +6,33 @@ package e_aloha.week3
  *
  * 2초이고 n 의 값이 15가 마지막이기 떄문에 시간적 여유가 있음
  */
+var max = Integer.MIN_VALUE
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
-    val timeArray = IntArray(n) { 0 }
-    val priceArray = IntArray(n) { 0 }
+    val timeAndPriceMap: HashMap<Int, Pair<Int, Int>> = hashMapOf()
 
     for (i in 0 until n) {
         val (time, price) = readLine().split(" ").map { it.toInt() }
-        timeArray[i] = time
-        priceArray[i] = price
+        timeAndPriceMap[i + 1] = Pair(time, price)
     }
 
-    val left = 0
-    val end = 0
+    //TODo for 문 1,2,....7
+    dfs(1, ,0, timeAndPriceMap)
 }
+
+fun dfs(depth: Int, sum: Int, timeAndPriceMap: HashMap<Int, Pair<Int, Int>>) {
+    if (depth > 7) {
+        return
+    }
+    for(i in depth+1 until 7+1) {
+        val (time,price) = timeAndPriceMap[i]!!
+        val nextDepth = depth + time
+        val nextSum = sum + price
+        // dfs(...)
+        max = maxOf(max, nextSum)
+    }
+
+
+}
+
+
