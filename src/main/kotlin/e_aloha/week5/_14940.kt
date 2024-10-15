5ade6252f52ce1dfe667ba3b77064abb80a3d489
jayking <mojunseo49@naver.com>
2024-10-15 13:13:16 +0900

    add: 5주차 문제풀이 리스트업

---
+++ src/main/kotlin/e_aloha/week5/_14940.kt
@@ -0,0 +1,9 @@
+package e_aloha.week5
+
+/**
+ * https://www.acmicpc.net/problem/14940
+ * 쉬운 최단 거리
+ */
+fun main() = with(System.`in`.bufferedReader()) {
+
+}
---
+++ src/main/kotlin/e_aloha/week5/_6064.kt
@@ -0,0 +1,10 @@
+package e_aloha.week5
+
+/**
+ * https://www.acmicpc.net/problem/6064
+ * 카잉 달력
+ *
+ */
+fun main() = with(System.`in`.bufferedReader()) {
+
+}
